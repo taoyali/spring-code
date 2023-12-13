e6c348c29540b1e14653b22976e5274dec1208d2
taoyali <taoyali@zepp.com>
2023-12-13 18:51:47 +0800

    feat |> 实现: 用户登录接口

--- src/main/java/com/tyl/controller/UserController.java
+++ src/main/java/com/tyl/controller/UserController.java
@@ -3,6 +3,7 @@ package com.tyl.controller;
 import com.tyl.pojo.Result;
 import com.tyl.pojo.User;
 import com.tyl.service.UserService;
+import com.tyl.utils.Md5Util;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,19 @@ public class UserController {
         } else {
             return Result.error("用户名已被占用");
         }
+    }
+
+    @PostMapping("/login")
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+        User user = userService.findByUserName(username);
+        if (user == null) {
+            return Result.error("用户不存在");
+        }
+
+        if (!Md5Util.getMD5String(password).equals(user.getPassword())) {
+            return Result.error("用户名或密码错误");
+        }
+
+        return Result.success("登录成功: jwt token 令牌");
     }
 }
