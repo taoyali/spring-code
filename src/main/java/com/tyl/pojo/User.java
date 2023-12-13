f1b9a9f83211265be77557d19c5522cac70f2d10
taoyali <taoyali@zepp.com>
2023-12-13 08:34:11 +0800

    feat |> 添加pojo对应的Result、User类

---
+++ src/main/java/com/tyl/pojo/User.java
@@ -0,0 +1,36 @@
+package com.tyl.pojo;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.time.LocalDateTime;
+
+@Data
+public class User {
+
+    @NotNull
+    private Integer id;
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    @NotNull
+    @Pattern(regexp = "^\\S{1,10}$")
+    private String nickname;
+
+    @NotNull
+    @Email
+    private String email;
+
+    private String userPic;
+
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+
+}
