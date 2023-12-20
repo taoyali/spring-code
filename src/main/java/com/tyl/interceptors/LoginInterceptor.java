722f7dcf04771b79fac34ef5fe9f91d0862271ad
taoyali <taoyali@zepp.com>
2023-12-20 13:53:42 +0800

    feat |> 实现接口的Token拦截

---
+++ src/main/java/com/tyl/interceptors/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.tyl.interceptors;
+
+import com.tyl.pojo.Result;
+import com.tyl.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    static private String tokenKey = "Authorization";
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 令牌验证
+        String token = request.getHeader(tokenKey);
+        try {
+            Map<String, Object> claims = JwtUtil.parseToken(token);
+            // 放行
+            return true;
+        } catch (Exception e) {
+            response.setStatus(401);
+            // 不放行
+            return false;
+        }
+    }
+}
