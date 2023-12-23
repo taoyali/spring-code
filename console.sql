435557e3f91489e660ac413709e46a2ebae3141b
taoyali <taoyali@zepp.com>
2023-12-23 10:52:12 +0800

    fix |> 修改数据库user表的password字段

--- console.sql
+++ console.sql
@@ -9,7 +9,7 @@ use big_event;
 create table user (
     id int unsigned primary key auto_increment comment 'ID',
     username varchar(20) not null unique comment '用户名',
-    password varchar(20) comment '密码',
+    password varchar(32) comment '密码',
     nickname varchar(10) default '' comment '昵称',
     email varchar(128) default '' comment '邮箱',
     user_pic varchar(128) default '' comment '头像',
